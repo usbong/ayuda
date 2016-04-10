7c2b5b03a88f4d7a263e10cc446a9db71d863938
Michael Syson <masarapmabuhay@gmail.com>
2016-04-10 09:43:15 +0800

    +fixed: Please separate the two paragraphs (untranslated and translated) into separate paragaphs

--- application/views/pages/Stories.php
+++ application/views/pages/Stories.php
@@ -145,7 +145,7 @@
 							</tr>
 							<tr width="100%">
 								<td colspan="2"><br>
-									<h5><i>Dako gid ang pasalamat ko na nakakadto ako ako sa Ayuda na event. Sadya gid kag madamu akong nakilala na pareho gid ang amun mga cause na ginatulungan! (I'm incredibly thankful that I was able to go to an Ayuda event. It was so fun and I met a lot of great people who had the exact same causes they wanted to help as me!)</i></h5><br><br>
+									<h5><i>Dako gid ang pasalamat ko na nakakadto ako ako sa Ayuda na event. Sadya gid kag madamu akong nakilala na pareho gid ang amun mga cause na ginatulungan! <br><br>(I'm incredibly thankful that I was able to go to an Ayuda event. It was so fun and I met a lot of great people who had the exact same causes they wanted to help as me!)</i></h5><br><br>
 								</td>
 							</tr>
 						</table>
@@ -206,7 +206,7 @@
 							</tr>
 							<tr width="100%">
 								<td colspan="2"><br>
-									<h5><i>Gusto jud ko mag boluntar pero wala pa ko kakita ug usa ka cause na geh ganahan jd nako. Maayo nalang na tungod na Ayuda, nakitan nako ang lahi-lahi na cause na duol sa ako mao dali na dayon maka pili. Di ko kahulat kung unsay sunod na panghitabo sa akoang syodad! (I've always wanted to volunteer but I’ve never had a particular cause that I cared about more than any other. It was great that Ayuda let me see my options for different causes near me so it was easier to make a decision where to volunteer. I can't wait for the next local event in my city!)</i></h5><br><br>
+									<h5><i>Gusto jud ko mag boluntar pero wala pa ko kakita ug usa ka cause na geh ganahan jd nako. Maayo nalang na tungod na Ayuda, nakitan nako ang lahi-lahi na cause na duol sa ako mao dali na dayon maka pili. Di ko kahulat kung unsay sunod na panghitabo sa akoang syodad! <br><br>(I've always wanted to volunteer but I’ve never had a particular cause that I cared about more than any other. It was great that Ayuda let me see my options for different causes near me so it was easier to make a decision where to volunteer. I can't wait for the next local event in my city!)</i></h5><br><br>
 								</td>
 							</tr>
 						</table>
@@ -233,7 +233,7 @@
 							</tr>
 							<tr width="100%">
 								<td colspan="2"><br>
-									<h5><i>Sobrang useful ng website! Di ko alam na sobra palang opportunidad na tumulong sa mga tao sa probinsya ng parents ko. First time ko pumunta sa Nueva Ecija at sobrang nakaconnect ako sa roots ko habang tumutulong sa pagturo sa mga bata. The website is so useful! I didn’t know that there were so many opportunities to help in my parent’s province. It’s the first time I went to Nueva Ecija and I super connected with my roots when I taught to the children!</i></h5><br><br>
+									<h5><i>Sobrang useful ng website! Di ko alam na sobra palang opportunidad na tumulong sa mga tao sa probinsya ng parents ko. First time ko pumunta sa Nueva Ecija at sobrang nakaconnect ako sa roots ko habang tumutulong sa pagturo sa mga bata. <br><br>(The website is so useful! I didn’t know that there were so many opportunities to help in my parent’s province. It’s the first time I went to Nueva Ecija and I super connected with my roots when I taught to the children!)</i></h5><br><br>
 								<br><br>
 								</td>
 							</tr>
